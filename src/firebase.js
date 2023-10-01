aa9bb9de188cec86ae48fead46dbba5fb070ba6c
Mateen Ahmed <136918338+VyperX2@users.noreply.github.com>
2023-10-01 17:30:18 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,7 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyAQr6nzysUfQpoy05MWW3urhcxvq0cn1ds",
-	authDomain: "blog-site-react.firebaseapp.com",
-	projectId: "blog-site-react",
-	storageBucket: "blog-site-react.appspot.com",
-	messagingSenderId: "690400282715",
-	appId: "1:690400282715:web:fff31db911f98e2f42577e",
-	measurementId: "G-543RQTC9JJ",
+	
 };
 
 const app = firebase.initializeApp(firebaseConfig)
